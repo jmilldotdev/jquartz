183b4d3721f1c379f708b10ab723bb37eaca9bbd
Jonathan Miller <jon@jmill.dev>
2025-11-09 11:01:43 +0100

    remove graph view

--- quartz.layout.ts
+++ quartz.layout.ts
@@ -39,11 +39,7 @@ export const defaultContentPageLayout: PageLayout = {
     Component.RandomPage(),
     Component.Explorer(),
   ],
-  right: [
-    Component.Graph(),
-    Component.DesktopOnly(Component.TableOfContents()),
-    Component.Backlinks(),
-  ],
+  right: [Component.DesktopOnly(Component.TableOfContents()), Component.Backlinks()],
 }
 
 // components for pages that display lists of pages  (e.g. tags or folders)
